50c5967b5a96bcd210ea8a9af372fdf44397b0d7
Serena Lertkantitham <lertkantitham@gmail.com>
2019-01-26 20:53:20 -0800

    post request

--- server.js
+++ server.js
@@ -18,6 +18,23 @@ app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
 })
 
+app.post('/blog-posts', jsonParser, (req, res) => {
+  // Check 'title', 'content', and 'author' are in body of request
+  const requiredFields = ["title", "content", "author"];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      res.status(400).send(message);
+    }
+  }
+
+  // create new blog post
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(post);
+})
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
